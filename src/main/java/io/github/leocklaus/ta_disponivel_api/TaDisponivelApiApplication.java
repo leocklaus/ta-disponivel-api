2499b7761ad8c9d87a850e18407cde08fc336c2f
leocklaus <leocklaus@gmail.com>
2024-08-26 11:46:58 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/ta_disponivel_api/TaDisponivelApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.ta_disponivel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaDisponivelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaDisponivelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/ta_disponivel_api/TaDisponivelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.ta_disponivel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaDisponivelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
